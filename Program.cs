e9a1f68dfb4fb725db4018d5d3d48c461dca1c82
rafalordi <rafaellordi@outlook.com.br>
2023-01-13 08:13:47 -0300

    primeiro commit

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Smartphone nokia = new Nokia("99 9 9999-9999", "Modelo 1", "1234567890", 128);
+Smartphone iphone = new Iphone("99 9 8888-7777", "Modelo 2", "1234567890", 128);
+
+
+Console.WriteLine("Teste Nokia");
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Teste IPhone");
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
